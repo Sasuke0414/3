ec71602d8ccfdb1ddb812f2aa06daad0bfe903ff
Abdulloh <163301172+Sasuke0414@users.noreply.github.com>
2024-08-27 15:11:20 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+Animal = str(input("Hayvon:"))
+height = int(input("Ogirligi:"))
+color = input("Rangi:")
+age = int(input("Yoshi:"))
+name = input("Ismi:")
+
+print(f"Hayvon: {Animal}, Og'irligi: {height}, Rangi: {color}, Yoshi: {age}, Ismi: {name} ")
